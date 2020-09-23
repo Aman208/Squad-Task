f35781b8f8675ab7eda45094785e9961b88efa55
Aman208 <amankumar20804@gmail.com>
2020-09-23 19:40:33 +0530

    first commit 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+
+
+import { Menu } from 'semantic-ui-react'
+
+const items = [
+  { key: 'editorials', active: true, name: 'Editorials' },
+  { key: 'review', name: 'Reviews' },
+  { key: 'events', name: 'Upcoming Events' },
+]
+
+const MenuExampleProps = () => <Menu items={items} />
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <MenuExampleProps/>
     </div>
   );
 }
